cd4e18fc5b5053e24f0e4d5c0b5b4f9aad427c00
nickaq <feswork7@gmail.com>
2024-03-25 09:45:39 +0100

    validation email with pass and parse email to UserName and UserSurname

--- app/src/main/java/com/example/androidenjoyer/AuthActivity.kt
+++ app/src/main/java/com/example/androidenjoyer/AuthActivity.kt
@@ -2,8 +2,10 @@ package com.example.androidenjoyer
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,16 +21,44 @@ class AuthActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        sendInfoToMainScreen()
+        validateEmailAndPass()
+    }
 
+    fun validateEmailAndPass() {
+        val buttonSend = findViewById<Button>(R.id.RegisterButton)
+        buttonSend.setOnClickListener {
+
+            //Pass Validation
+            val passText = findViewById<EditText>(R.id.PasswordTextEditing)
+            val passTextToString = passText.text.toString()
+            val validationPatternForPass = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}\$"
+            val isMatchPass = Regex(validationPatternForPass).matches(passTextToString)
+            if (!isMatchPass) passText.error = "min 8 characters 1 letter and 1 number"
+
+            // Email Validation
+            val emailText = findViewById<EditText>(R.id.EmailTextEditing)
+            val emailTextToString = emailText.text.toString()
+            val validationPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$"
+            val isMatchEmail = Regex(validationPattern).matches(emailTextToString)
+            if (!isMatchEmail) emailText.error = "test@gmail.com example"
+
+            if (isMatchEmail && isMatchPass) sendInfoToMainScreen()
+        }
     }
 
     fun sendInfoToMainScreen() {
         val buttonSend = findViewById<Button>(R.id.RegisterButton)
         buttonSend.setOnClickListener {
             val emailText = findViewById<EditText>(R.id.EmailTextEditing)
+            val beforeParseEmail = emailText
+                .text
+                .toString()
+                .trim()
+                .substringBefore("@")
+                .split(".")
+                .joinToString(" ") { word -> word.replaceFirstChar { it.uppercase() } }
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("email", emailText.text.toString())
+            intent.putExtra("email", beforeParseEmail)
             startActivity(intent)
         }
     }
