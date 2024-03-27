9b1159f6ce99242fedbf64db4e848420c0066a35
nickaq <feswork7@gmail.com>
2024-03-27 16:37:25 +0100

    New changes

--- app/src/main/java/com/example/androidenjoyer/AuthActivity.kt
+++ app/src/main/java/com/example/androidenjoyer/AuthActivity.kt
@@ -10,12 +10,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidenjoyer.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -25,43 +28,34 @@ class AuthActivity : AppCompatActivity() {
     }
 
     fun validateEmailAndPass() {
-        val buttonSend = findViewById<Button>(R.id.RegisterButton)
-        buttonSend.setOnClickListener {
-
+        binding.RegisterButton.setOnClickListener {
             //Pass Validation
-            val passText = findViewById<EditText>(R.id.PasswordTextEditing)
-            val passTextToString = passText.text.toString()
             val validationPatternForPass = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}\$"
-            val isMatchPass = Regex(validationPatternForPass).matches(passTextToString)
-            if (!isMatchPass) passText.error = "min 8 characters 1 letter and 1 number"
+            val isMatchPass = Regex(validationPatternForPass).matches(binding.PasswordTextEditing.text.toString())
+            if (!isMatchPass) binding.PasswordTextEditing.error =
+                "min 8 characters 1 letter and 1 number"
 
             // Email Validation
-            val emailText = findViewById<EditText>(R.id.EmailTextEditing)
-            val emailTextToString = emailText.text.toString()
-            val validationPattern = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$"
-            val isMatchEmail = Regex(validationPattern).matches(emailTextToString)
-            if (!isMatchEmail) emailText.error = "test@gmail.com example"
+            val validationPattern = "^[a-z0-9]+[.][a-z0-9]+@gmail\\.com\$"
+            val isMatchEmail = Regex(validationPattern).matches(binding.EmailTextEditing.text.toString())
+            if (!isMatchEmail) binding.EmailTextEditing.error = "test@gmail.com example"
 
-            if (isMatchEmail && isMatchPass) sendInfoToMainScreen()
-        }
-    }
-
-    fun sendInfoToMainScreen() {
-        val buttonSend = findViewById<Button>(R.id.RegisterButton)
-        buttonSend.setOnClickListener {
-            val emailText = findViewById<EditText>(R.id.EmailTextEditing)
-            val beforeParseEmail = emailText
-                .text
-                .toString()
-                .trim()
-                .substringBefore("@")
-                .split(".")
-                .joinToString(" ") { word -> word
-                    .lowercase()
-                    .replaceFirstChar { it.uppercase() } }
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("email", beforeParseEmail)
-            startActivity(intent)
+            if (isMatchEmail && isMatchPass) {
+                val beforeParseEmail = binding.EmailTextEditing
+                    .text
+                    .toString()
+                    .trim()
+                    .substringBefore("@")
+                    .split(".")
+                    .joinToString(" ") { word ->
+                        word
+                            .lowercase()
+                            .replaceFirstChar { it.uppercase() }
+                    }
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("email", beforeParseEmail)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/androidenjoyer/MainActivity.kt
+++ app/src/main/java/com/example/androidenjoyer/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.androidenjoyer
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidenjoyer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         val userNameText = intent.getStringExtra("email")
-        val textView = findViewById<TextView>(R.id.userName)
-        textView.text = userNameText
+        binding.userNameText.text = userNameText
     }
 }
